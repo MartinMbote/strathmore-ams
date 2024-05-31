3c620fc85dd5b3636494b49f3f1d83befb5b2a9e
Martin <martinmmbote@gmail.com>
2024-05-31 20:12:40 +0300

    nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { strathLogo, downArrow } from '../assets'
 
 const Navbar = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [rotation, setRotation] = useState(0);
+
+  const rotateImage = () => {
+    setRotation(rotation + 180);
+    setIsVisible(!isVisible);
+  }
 
   const navBarContents = [
     {
       title : "Online Resources",
       titleContent : ["University Website", "AMS Students Module", "AMS Lecturers Module", "Attachment System", "Clearance System", "University Intranet", "Safe Exam Browser", "Password Reset"]
+    },
+
+    {
+      title : "University Library",
+      titleContent : ["Placeholder"]
+    },
+
+    {
+      title : "SBS Learning",
+      titleContent : ["Placeholder"]
+    },
+
+    {
+      title : "Mape Learning",
+      titleContent : ["Placeholder"]
+    },
+
+    {
+      title : "Need Assistance",
+      titleContent : ["Placeholder"]
     }
   ];
 
@@ -20,27 +48,33 @@ const Navbar = () => {
           
             {navBarContents.map((section, index) => (
               <div>
-                <div className='text-[1vw] font-semibold text-white flex gap-[0.3vw] mt-[3vw]' key={index}>
+                <div className='text-[1vw] font-semibold text-white flex gap-[0.3vw] mt-[3vw] mr-[3vw] cursor-pointer' key={index} onClick={rotateImage}>
                   <p>
                     {section.title}
                   </p>
 
-                  <img src={downArrow} className='w-[1.3vw] h-[1.3vw] mt-[0.3vw]' />
+                  <img src={downArrow} className='w-[1.3vw] h-[1.3vw] mt-[0.3vw] transition ease-in-out duration-600'
+                    style={{
+                      transform: `rotate(${rotation}deg)`
+                    }}                    
+                  />
                 </div>
+              
+                {isVisible && (
+                  <div className='absolute z-10 mt-[0.4vw] ml-[-1.5vw] text-center rounded-[0.2vw] text-[1vw] w-[12vw] font-normal bg-white'>
+                    {section.titleContent.map((content, i) => (
+                      <div key={i}>
+                        <p className='my-[0.4vw]'>
+                          {section.titleContent[i]}
+                        </p>
 
-                <div className='absolute z-10 mt-[0.4vw] ml-[-0.5vw] text-center rounded-[0.2vw] text-[1vw] px-[1vw] font-normal bg-white'>
-                  {section.titleContent.map((content, i) => (
-                    <div key={i}>
-                      <p className='my-[0.4vw]'>
-                        {section.titleContent[i]}
-                      </p>
-
-                      <div className='flex justify-center'>
-                        <div className='w-[3vw] h-[0.05vw] bg-gray-300'></div>
+                        <div className='flex justify-center'>
+                          <div className='w-[3vw] h-[0.05vw] bg-gray-300'></div>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </div>
+                    ))}
+                  </div>
+                )}
             
               </div>
             ))}
