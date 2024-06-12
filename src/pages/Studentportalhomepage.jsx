050951af12e01e4b36af5b8b8e392df617005d46
Martin <martinmmbote@gmail.com>
2024-06-13 00:27:51 +0300

    dsd

--- src/pages/Studentportalhomepage.jsx
+++ src/pages/Studentportalhomepage.jsx
@@ -8,9 +8,9 @@ const Studentportalhomepage = () => {
     <div>
         <NavbarStudentPortal />
 
-        <div className='fixed'>
+        <div className='fixed left-[15vw]'>
           <div className='w-full h-[43.8vw] opacity-50 bg-nav-blue absolute'></div>
-          <img src={bgImage} className='h-[43.85vw] w-[100vw]' />
+          <img src={bgImage} className='h-[43.85vw] w-[85vw]' />
         </div>
 
         <SidePanel />
